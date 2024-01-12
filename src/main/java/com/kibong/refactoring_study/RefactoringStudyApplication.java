1652852cd82d75c86aa8d93b19edbfc13204f57f
kibong95 <kibongcoders>
2024-01-13 00:28:00 +0900

    Initial commit

---
+++ src/main/java/com/kibong/refactoring_study/RefactoringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.kibong.refactoring_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactoringStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kibong/refactoring_study/RefactoringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibong.refactoring_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
