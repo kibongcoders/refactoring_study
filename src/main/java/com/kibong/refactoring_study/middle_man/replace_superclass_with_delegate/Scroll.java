8906640f2ace38bc75947bde03675277ef87ed1e
kibongcorders <kkp02052@gmail.com>
2024-02-11 21:53:33 +0900

    슈퍼클래스를 위임으로 바꾸기(Replace Superclass With Delegate)

---
+++ src/main/java/com/kibong/refactoring_study/middle_man/replace_superclass_with_delegate/Scroll.java
@@ -0,0 +1,20 @@
+package com.kibong.refactoring_study.middle_man.replace_superclass_with_delegate;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class Scroll{
+
+    private LocalDate dateLastCleaned;
+    private CategoryItem categoryItem;
+
+    public Scroll(Integer id, String title, List<String> tags, LocalDate dateLastCleaned) {
+        this.dateLastCleaned = dateLastCleaned;
+        this.categoryItem = new CategoryItem(id, title, tags);
+    }
+
+    public long daysSinceLastCleaning(LocalDate targetDate) {
+        return this.dateLastCleaned.until(targetDate, ChronoUnit.DAYS);
+    }
+}
